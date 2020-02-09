daec3369b60fa3db139efccc417e529d7d9d6466
Levi Porto <leviportofigueiredo@gmail.com>
2020-02-09 11:14:13 -0300

    Cleaning code

--- src/main/java/com/levi/weatherman/service/SolService.java
+++ src/main/java/com/levi/weatherman/service/SolService.java
@@ -2,7 +2,6 @@ package com.levi.weatherman.service;
 
 import com.levi.weatherman.exception.SolDoesNotExistException;
 import com.levi.weatherman.client.NasaRestTemplate;
-import com.levi.weatherman.dto.SolDTO;
 import com.levi.weatherman.dto.SolTemperatureDTO;
 import com.levi.weatherman.extractor.SolExtractor;
 import org.springframework.http.HttpEntity;
@@ -10,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class SolService {
@@ -22,31 +22,35 @@ public class SolService {
         this.solExtractor = solExtractor;
     }
 
-    @SuppressWarnings("rawtypes")
+    @SuppressWarnings({"unchecked", "rawtypes"})
     public List<SolTemperatureDTO> findTemperaturesBySol(String sol) throws SolDoesNotExistException {
         HttpEntity<LinkedHashMap> response = nasaRestTemplate.getSolsInformationFromNasa();
-        LinkedHashMap body = response.getBody();
+        LinkedHashMap<String, Object> body = response.getBody();
         if(!StringUtils.isEmpty(sol)) {
-            if(Objects.requireNonNull(body).containsKey(sol)) {
-                SolDTO solDTO = solExtractor.extractSol(body, sol);
-                return Collections.singletonList(findTemperatureBySol(sol, solDTO));
-            } else {
-                throw new SolDoesNotExistException("This SOL is not available more");
-            }
+            return getSpecificSolIfExist(sol, body);
         } else {
-            List<SolTemperatureDTO> solTemperatureDTOS = new ArrayList<>();
-            for(Object object : (ArrayList) Objects.requireNonNull(body).get("sol_keys")) {
-                SolDTO solDTO = solExtractor.extractSol(body, (String) object);
-                solTemperatureDTOS.add(findTemperatureBySol((String) object, solDTO));
-            }
-            return solTemperatureDTOS;
+            return getAllAvailableSolsTemperature(body);
         }
-
-
     }
 
-    public SolTemperatureDTO findTemperatureBySol(String sol, SolDTO solDTO) {
-        return SolTemperatureDTO.builder().Sol(sol).averageTemperature(solDTO.getAtDTO().getAv()).build();
+    private List<SolTemperatureDTO> getSpecificSolIfExist(String sol, LinkedHashMap<String, Object> body) {
+        if(Objects.requireNonNull(body).containsKey(sol)) {
+            return Collections.singletonList(constructSolTemperatureDTO(body, sol));
+        } else {
+            throw new SolDoesNotExistException("This SOL is not available more");
+        }
     }
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    private List<SolTemperatureDTO> getAllAvailableSolsTemperature(LinkedHashMap<String, Object> body) {
+        ArrayList<Object> availableSols = (ArrayList) Objects.requireNonNull(body).get("sol_keys");
+        return availableSols.stream().map(object -> constructSolTemperatureDTO(body, (String) object)).collect(Collectors.toList());
+    }
+
+    private SolTemperatureDTO constructSolTemperatureDTO(LinkedHashMap<String, Object> body, String object) {
+        return SolTemperatureDTO.builder().Sol(object)
+                .averageTemperature(solExtractor.extractSol(body, object).getAtDTO().getAv()).build();
+    }
+
 
 }
